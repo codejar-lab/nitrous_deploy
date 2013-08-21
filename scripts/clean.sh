c90ff92aabc2ee14b815b640eb368e3d160ea2c0
Arpan Chavda <arpanchavdaeng@gmail.com>
2013-08-21 07:41:28 +0000

    clean script updated

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-cd /home/action/workspace
+cd /home/action/
 mem=`du -sh | cut -f1 -d "M"`
-echo "used memory : $mem"
-if [ $mem -gt 130 ]; then
+#echo "used memory : $mem"
+if [ $mem -gt 800 ]; then
 	{	
-		echo "Removing database"
-		#rm -rf /home/action/workspace/library/calibre
-	}
-else
-	{
-		echo "Yep..you have more memory"
+		#echo "Removing database"
+		rm -rf /home/action/workspace/library/calibre
 	}
 fi
 
