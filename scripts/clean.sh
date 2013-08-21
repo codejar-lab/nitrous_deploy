ded3fdbb807b1c440ea446e9635f4d5fb21150cc
Arpan Chavda <arpanchavdaeng@gmail.com>
2013-08-21 16:30:09 +0530

    data increased

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,7 +2,7 @@
 cd /home/action/
 mem=`du -sh | cut -f1 -d "M"`
 #echo "used memory : $mem"
-if [ $mem -gt 800 ]; then
+if [ $mem -gt 1200 ]; then
 	{	
 		#echo "Removing database"
 		rm -rf /home/action/workspace/library/calibre
