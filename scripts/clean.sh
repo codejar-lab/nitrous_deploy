589fa958af3d41ee5063933ac77801b186b0b16f
Arpan Chavda <arpanchavdaeng@gmail.com>
2013-08-27 10:05:03 +0530

    merge of removal script;cron update added

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,10 +2,16 @@
 cd /home/action/
 mem=`du -sh | cut -f1 -d "M"`
 #echo "used memory : $mem"
-if [ $mem -gt 1300 ]; then
+if [ $mem -gt 1200 ]; then
 	{	
+		cd ~
+		./workspace/calibre/calibredb list --search 'date:<150daysago and tags:news' | cut -f1 -d " " > tmp.txt
+		sed -i '1,1d' tmp.txt 
+		sed -i '$d' tmp.txt 
+		while read line; do ./workspace/calibre/calibredb remove $line; done < tmp.txt
+		rm tmp.txt
 		#echo "Removing database"
-		rm -rf /home/action/workspace/library/calibre
+		#rm -rf /home/action/workspace/library/calibre
 	}
 fi
 
